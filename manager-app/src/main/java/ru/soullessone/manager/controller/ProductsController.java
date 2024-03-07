f10f7b31b55a26435d12bcb79d10f904131dc5de
soullessOne <rustamrazabekov1705@gmail.com>
2024-03-07 14:40:23 +0600

    Adding new method of adding a product in ProductController, new template for this method new_product.html

--- manager-app/src/main/java/ru/soullessone/manager/controller/ProductsController.java
+++ manager-app/src/main/java/ru/soullessone/manager/controller/ProductsController.java
@@ -4,6 +4,7 @@ package ru.soullessone.manager.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.soullessone.manager.service.ProductService;
@@ -15,9 +16,14 @@ public class ProductsController {
 
     private final ProductService productService;
 
-    @RequestMapping(value = "list", method = RequestMethod.GET)
+    @GetMapping("list")
     public String getProductsList(Model model){
         model.addAttribute("products", this.productService.findAllProducts());
         return "catalogue/products/list";
+    }
+
+    @GetMapping
+    public String getNewProductPage(){
+        return "catalogue/products/new_product";
     }
 }
